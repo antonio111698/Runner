0190a3ca7d6cc209ec292ca81097ba9b726c530a
Antonio Artimon <antonio_artimon@yahoo.com>
2024-05-28 22:36:19 +0300

    Creating the Material Theme

--- app/src/main/java/com/tonio/runner/MainActivity.kt
+++ app/src/main/java/com/tonio/runner/MainActivity.kt
@@ -10,7 +10,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.tonio.runner.ui.theme.RunnerTheme
+import com.tonio.core.presentation.designsystem.RunnerTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
